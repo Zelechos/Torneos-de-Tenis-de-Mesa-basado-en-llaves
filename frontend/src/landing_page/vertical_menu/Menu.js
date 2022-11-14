c01c1d25ce610db5e682f2a16e93f7add92d9844
Ariel Colque Garcia <colqueariel023@gmail.com>
2022-11-14 18:29:53 -0400

    feat: create the vertical menu

---
+++ frontend/src/landing_page/vertical_menu/Menu.js
@@ -0,0 +1,23 @@
+import './Menu.css';
+
+function Menu() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Menu
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Menu;
