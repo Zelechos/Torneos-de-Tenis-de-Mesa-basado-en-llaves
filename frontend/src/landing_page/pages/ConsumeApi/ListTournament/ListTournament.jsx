9cf756c840df290e920c3d71b2eac2209ebccdbb
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2022-12-01 13:58:10 -0400

    feat: add table tournament

--- frontend/src/landing_page/pages/ConsumeApi/ListTournament/ListTournament.jsx
+++ frontend/src/landing_page/pages/ConsumeApi/ListTournament/ListTournament.jsx
@@ -1,5 +1,54 @@
+import Table from "react-bootstrap/Table";
+
 function ListTournament() {
-  return <h1>Lista de Torneos</h1>;
+  return (
+    <>
+      <h1 className="title-table">Tabla Torneos</h1>
+
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Administrador</th>
+            <th>Categoria</th>
+            <th>Descripcion</th>
+            <th>Sede</th>
+            <th>Fecha Inicio</th>
+            <th>Fecha Final</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Rey</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>Rey</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>Rey</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+            <td>@mdo</td>
+          </tr>
+        </tbody>
+      </Table>
+    </>
+  );
 }
 
 export default ListTournament;
