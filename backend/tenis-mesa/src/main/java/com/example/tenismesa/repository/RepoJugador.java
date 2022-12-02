6df475d016fc94aa167463ec5f22a7866460a3b9
Luis99DavidFloresTorres <luisdavid99florestorres@gmail.com>
2022-12-02 18:01:07 -0400

    fixed error player

--- backend/tenis-mesa/src/main/java/com/example/tenismesa/repository/RepoJugador.java
+++ backend/tenis-mesa/src/main/java/com/example/tenismesa/repository/RepoJugador.java
@@ -4,5 +4,4 @@ import com.example.tenismesa.models.Jugador;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RepoJugador extends JpaRepository<Jugador,Long> {
-    Jugador findByNombre(String nombre);
 }
