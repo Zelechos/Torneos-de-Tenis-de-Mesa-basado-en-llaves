19675d777107b6caf779a9536c58a236c8f75417
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2022-12-03 19:14:26 -0400

    feat: POST player connection completed

---
+++ frontend/src/landing_page/pages/ConsumeApi/Modal.jsx
@@ -0,0 +1,20 @@
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+
+function ModalAlert({ show, handleClose, mensaje }) {
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Alerta</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>{mensaje}</Modal.Body>
+      <Modal.Footer>
+        <Button variant={show ? "danger" : "primary"} onClick={handleClose}>
+          OK
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+export default ModalAlert;
--- frontend/src/landing_page/pages/ConsumeApi/Roles/Jugador/FormJugador.jsx
+++ frontend/src/landing_page/pages/ConsumeApi/Roles/Jugador/FormJugador.jsx
@@ -1,8 +1,12 @@
 import { useState } from "react";
 import Form from "react-bootstrap/Form";
+import ModalAlert from "../../Modal";
 import JugadorImg from "./img-jugador.jpg";
 
 function FormJugador() {
+  const [show, setShow] = useState(false);
+  const [mensaje, setMensaje] = useState("");
+
   const [player, setPlayer] = useState({
     altura: 0,
     apellidos: "",
@@ -35,7 +39,7 @@ function FormJugador() {
       parseInt(player.altura) <= 0 ||
       parseInt(player.ranking) < 0
     ) {
-      alert("Todos los campos deben ser llenados");
+      setMensaje("Todos los campos deben ser llenados");
     } else {
       // consulta
       const requestInit = {
@@ -43,6 +47,7 @@ function FormJugador() {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(player),
       };
+
       fetch("http://localhost:9090/api/jugador/add", requestInit)
         .then((respuesta) => respuesta.json())
         .then((respuesta) => console.log(respuesta));
@@ -59,131 +64,139 @@ function FormJugador() {
         nombre: "",
         ranking: 0,
       });
+      setMensaje("Se registro el Jugador");
+      handleShow();
     }
   };
 
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+
   return (
-    <form className="main" onSubmit={handleSubmit}>
-      <div className="container-img">
-        <img className="jugador-img" src={JugadorImg} alt="" />
-      </div>
-      <div className="data-jugador">
-        <h1 className="heading">Registro Jugador</h1>
-        <div className="input">
-          <label for="nombre">Nombres: </label>
-          <input
-            onChange={handleChange}
-            type="text"
-            name="nombre"
-            autocomplete="false"
-            value={player.nombre}
-          />
+    <>
+      <form className="main" onSubmit={handleSubmit}>
+        <div className="container-img">
+          <img className="jugador-img" src={JugadorImg} alt="" />
         </div>
-        <div className="input">
-          <label for="apellidos">Apellidos: </label>
-          <input
-            onChange={handleChange}
-            type="text"
-            name="apellidos"
-            autocomplete="false"
-            value={player.apellidos}
-          />
-        </div>
-
-        <div className="input">
-          <label for="nacionalidad">Nacionalidad: </label>
-          <input
-            onChange={handleChange}
-            type="text"
-            name="nacionalidad"
-            autocomplete="false"
-            value={player.nacionalidad}
-          />
-        </div>
-
-        <div className="input two-option">
-          <div className="label-fecha">
-            <label for="fecha_nacimiento">Fecha Nacimiento:</label>
+        <div className="data-jugador">
+          <h1 className="heading">Registro Jugador</h1>
+          <div className="input">
+            <label for="nombre">Nombres: </label>
             <input
               onChange={handleChange}
-              type="date"
-              name="fecha_nacimiento"
+              type="text"
+              name="nombre"
               autocomplete="false"
-              value={player.fecha_nacimiento}
+              value={player.nombre}
             />
           </div>
-          <div>
-            <label for="ranking">Ranking: </label>
+          <div className="input">
+            <label for="apellidos">Apellidos: </label>
             <input
               onChange={handleChange}
-              type="number"
-              name="ranking"
+              type="text"
+              name="apellidos"
               autocomplete="false"
-              value={player.ranking}
+              value={player.apellidos}
             />
           </div>
-        </div>
 
-        <div className="input two-option">
-          <div>
-            <label for="altura">Altura (cm): </label>
+          <div className="input">
+            <label for="nacionalidad">Nacionalidad: </label>
             <input
               onChange={handleChange}
-              type="number"
-              name="altura"
+              type="text"
+              name="nacionalidad"
               autocomplete="false"
-              value={player.altura}
+              value={player.nacionalidad}
             />
           </div>
-          <div>
-            <label for="peso">Peso (Kg): </label>
-            <input
-              onChange={handleChange}
-              type="number"
-              name="peso"
-              autocomplete="false"
-              value={player.peso}
-            />
-          </div>
-        </div>
 
-        <div className="input two-option">
-          <div className="container-select">
-            <label for="skilled-hand">Mano Habil: </label>
-            <Form.Select
-              aria-label="Default select example"
-              className="select"
-              onChange={handleChange}
-              name="mano_habil"
-              value={player.mano_habil}
-            >
-              <option value="">Ver opciones</option>
-              <option value="D">Diestro</option>
-              <option value="Z">Zurdo</option>
-              <option value="A">Ambos</option>
-            </Form.Select>
+          <div className="input two-option">
+            <div className="label-fecha">
+              <label for="fecha_nacimiento">Fecha Nacimiento:</label>
+              <input
+                onChange={handleChange}
+                type="date"
+                name="fecha_nacimiento"
+                autocomplete="false"
+                value={player.fecha_nacimiento}
+              />
+            </div>
+            <div>
+              <label for="ranking">Ranking: </label>
+              <input
+                onChange={handleChange}
+                type="number"
+                name="ranking"
+                autocomplete="false"
+                value={player.ranking}
+              />
+            </div>
           </div>
 
-          <div className="container-select">
-            <label for="genro">Genero: </label>
-            <Form.Select
-              aria-label="Default select example"
-              className="select"
-              onChange={handleChange}
-              name="sexo"
-              value={player.sexo}
-            >
-              <option value="">Ver opciones</option>
-              <option value="M">Masculino</option>
-              <option value="F">Femenino</option>
-            </Form.Select>
+          <div className="input two-option">
+            <div>
+              <label for="altura">Altura (cm): </label>
+              <input
+                onChange={handleChange}
+                type="number"
+                name="altura"
+                autocomplete="false"
+                value={player.altura}
+              />
+            </div>
+            <div>
+              <label for="peso">Peso (Kg): </label>
+              <input
+                onChange={handleChange}
+                type="number"
+                name="peso"
+                autocomplete="false"
+                value={player.peso}
+              />
+            </div>
           </div>
+
+          <div className="input two-option">
+            <div className="container-select">
+              <label for="skilled-hand">Mano Habil: </label>
+              <Form.Select
+                aria-label="Default select example"
+                className="select"
+                onChange={handleChange}
+                name="mano_habil"
+                value={player.mano_habil}
+              >
+                <option value="">Ver opciones</option>
+                <option value="D">Diestro</option>
+                <option value="Z">Zurdo</option>
+                <option value="A">Ambos</option>
+              </Form.Select>
+            </div>
+
+            <div className="container-select">
+              <label for="genro">Genero: </label>
+              <Form.Select
+                aria-label="Default select example"
+                className="select"
+                onChange={handleChange}
+                name="sexo"
+                value={player.sexo}
+              >
+                <option value="">Ver opciones</option>
+                <option value="M">Masculino</option>
+                <option value="F">Femenino</option>
+              </Form.Select>
+            </div>
+          </div>
+          <button className="btn-register" type="submit" onClick={handleShow}>
+            Registrar Jugador
+          </button>
         </div>
-        <button className="btn-register" type="submit">
-          Registrar Jugador
-        </button>
-      </div>
-    </form>
+      </form>
+      <ModalAlert show={show} handleClose={handleClose} mensaje={mensaje} />
+    </>
   );
 }
 
