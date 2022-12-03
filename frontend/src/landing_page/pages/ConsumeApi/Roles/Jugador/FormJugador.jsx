9b0cdc55f3175b7063e720e1c69c7ef3016cf528
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2022-12-03 14:21:47 -0400

    feat: add method POST in Form Player

--- frontend/src/landing_page/pages/ConsumeApi/Roles/Jugador/FormJugador.jsx
+++ frontend/src/landing_page/pages/ConsumeApi/Roles/Jugador/FormJugador.jsx
@@ -17,53 +17,93 @@ function FormJugador() {
 
   const handleChange = (e) => {
     setPlayer({
+      ...player,
       [e.target.name]: e.target.value,
     });
   };
 
+  const handleSubmit = () => {
+    // validar datos
+    if (
+      player.nombre === "" ||
+      player.apellidos === "" ||
+      player.nacionalidad === "" ||
+      player.sexo === "" ||
+      player.mano_habil === "" ||
+      parseInt(player.peso) <= 0 ||
+      parseFloat(player.altura) <= 0 ||
+      parseInt(player.ranking) < 0
+    ) {
+      alert("Todos los campos deben ser llenados");
+    } else {
+      // consulta
+      const requestInit = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(player),
+      };
+      fetch("http://localhost:9090/api/jugador/add", requestInit)
+        .then((respuesta) => respuesta.json())
+        .then((respuesta) => console.log(respuesta));
+
+      // reiniciando state del jugador
+      setPlayer({
+        altura: 0,
+        apellidos: "",
+        fecha_nacimiento: "",
+        mano_habil: "",
+        nacionalidad: "",
+        peso: 0,
+        sexo: "",
+        nombre: "",
+        ranking: 0,
+      });
+    }
+  };
+
   return (
-    <form className="main">
+    <form className="main" onSubmit={handleSubmit}>
       <div className="container-img">
         <img className="jugador-img" src={JugadorImg} alt="" />
       </div>
       <div className="data-jugador">
         <h1 className="heading">Registro Jugador</h1>
         <div className="input">
-          <label for="name">Nombres: </label>
+          <label for="nombre">Nombres: </label>
           <input
             onChange={handleChange}
             type="text"
-            name="name"
+            name="nombre"
             autocomplete="false"
           />
         </div>
         <div className="input">
-          <label for="lastname">Apellidos: </label>
+          <label for="apellidos">Apellidos: </label>
           <input
             onChange={handleChange}
             type="text"
-            name="lastname"
+            name="apellidos"
             autocomplete="false"
           />
         </div>
 
         <div className="input">
-          <label for="nationality">Nacionalidad: </label>
+          <label for="nacionalidad">Nacionalidad: </label>
           <input
             onChange={handleChange}
             type="text"
-            name="nationality"
+            name="nacionalidad"
             autocomplete="false"
           />
         </div>
 
         <div className="input two-option">
           <div className="label-fecha">
-            <label for="date-birth">Fecha Nacimiento:</label>
+            <label for="fecha_nacimiento">Fecha Nacimiento:</label>
             <input
               onChange={handleChange}
               type="date"
-              name="date-birth"
+              name="fecha_nacimiento"
               autocomplete="false"
             />
           </div>
@@ -80,20 +120,20 @@ function FormJugador() {
 
         <div className="input two-option">
           <div>
-            <label for="height">Altura (mts): </label>
+            <label for="altura">Altura (mts): </label>
             <input
               onChange={handleChange}
               type="number"
-              name="height"
+              name="altura"
               autocomplete="false"
             />
           </div>
           <div>
-            <label for="weight">Peso (Kg): </label>
+            <label for="peso">Peso (Kg): </label>
             <input
               onChange={handleChange}
               type="number"
-              name="weight"
+              name="peso"
               autocomplete="false"
             />
           </div>
@@ -102,7 +142,13 @@ function FormJugador() {
         <div className="input two-option">
           <div className="container-select">
             <label for="skilled-hand">Mano Habil: </label>
-            <Form.Select aria-label="Default select example" className="select">
+            <Form.Select
+              aria-label="Default select example"
+              className="select"
+              onChange={handleChange}
+              name="mano_habil"
+            >
+              <option value="">Ver opciones</option>
               <option value="D">Diestro</option>
               <option value="Z">Zurdo</option>
               <option value="A">Ambos</option>
@@ -111,13 +157,21 @@ function FormJugador() {
 
           <div className="container-select">
             <label for="genro">Genero: </label>
-            <Form.Select aria-label="Default select example" className="select">
+            <Form.Select
+              aria-label="Default select example"
+              className="select"
+              onChange={handleChange}
+              name="sexo"
+            >
+              <option value="">Ver opciones</option>
               <option value="M">Masculino</option>
               <option value="F">Femenino</option>
             </Form.Select>
           </div>
         </div>
-        <button className="btn-register">Registrar Jugador</button>
+        <button className="btn-register" type="submit">
+          Registrar Jugador
+        </button>
       </div>
     </form>
   );
