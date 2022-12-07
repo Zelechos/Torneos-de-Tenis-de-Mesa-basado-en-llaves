844a24c1b89e7f29287741630f7f91bfca199e82
Argus Aphocraphex <zelechos@hotmail.com>
2022-12-06 21:24:19 -0400

    feat: add the keys

--- frontend/src/landing_page/pages/ConsumeApi/ListPartidos/ListPartidos.jsx
+++ frontend/src/landing_page/pages/ConsumeApi/ListPartidos/ListPartidos.jsx
@@ -1,14 +1,31 @@
 import "./stylesPartidos.css";
+import { useState, useEffect } from "react";
 
 function ListPartidos() {
+  const [players, setPlayers] = useState([]);
+  const [listUpdate, setlistUpdate] = useState(false);
+
+  useEffect(() => {
+    const GetPlayers = () => {
+      fetch("http://localhost:9090/api/arbitro/mostrar")
+        .then((respuesta) => respuesta.json())
+        .then((respuesta) => setPlayers(respuesta));
+    };
+    GetPlayers();
+    setlistUpdate(false);
+  }, [listUpdate]);
+
   return (
     <>
-    <h1>Lista de Partidos LLaves</h1>
-    <ul>
-      <li>Brasil</li>
-    <hr className="line"/>
-      <li>Marruecos</li>
-    </ul>
+      <h1 className="title-table">LLaves</h1>
+      <ul>
+        {players.map((player) => (
+          <>
+            <li>{player["p1"].nombre}</li>
+            <li>{player["p2"].nombre}</li>
+          </>
+        ))}
+      </ul>
     </>
   );
 }
