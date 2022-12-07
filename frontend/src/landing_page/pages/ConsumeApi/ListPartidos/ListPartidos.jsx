79e32eed24b9504d9c7738b632c7f91dcbe9c11c
Alex Tumiri Huanca <zelechos@hotmail.com>
2022-12-06 20:33:32 -0400

    feat: add the styles of matchs

--- frontend/src/landing_page/pages/ConsumeApi/ListPartidos/ListPartidos.jsx
+++ frontend/src/landing_page/pages/ConsumeApi/ListPartidos/ListPartidos.jsx
@@ -1,5 +1,16 @@
+import "./stylesPartidos.css";
+
 function ListPartidos() {
-  return <h1>Lista de Partidos</h1>;
+  return (
+    <>
+    <h1>Lista de Partidos LLaves</h1>
+    <ul>
+      <li>Brasil</li>
+    <hr className="line"/>
+      <li>Marruecos</li>
+    </ul>
+    </>
+  );
 }
 
 export default ListPartidos;
